4af2c06072660ee5fe40786899dea6925d4da077
Nils Ole Timm <nils.ole.timm@googlemail.com>
2021-05-29 18:29:29 +0200

    Removed unnecessary output

--- measure_test.py
+++ measure_test.py
@@ -7,15 +7,10 @@ loco = Locomotion()
 sense = Sensor(config.SENSOR1_TRIGGER, config.SENSOR1_ECHO, config.SENSOR1_MAXDIST)
 result = ""
 
-try:
-    while True:
-        loco.tightLeft()
-        time.sleep(0.01)
-        loco.stop()
-        time.sleep(0.2)
-        dist = sense.measure()
-        print(dist)
-        result +=","
-        result += str(dist)
-except:
-    print(result)
+while True:
+    loco.tightLeft()
+    time.sleep(0.01)
+    loco.stop()
+    time.sleep(0.2)
+    dist = sense.measure()
+    print(dist)
