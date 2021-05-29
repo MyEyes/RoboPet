d6b58f7c73631677678e7f0199e0d62b3d228bd1
Nils Ole Timm <nils.ole.timm@googlemail.com>
2021-05-29 18:01:04 +0200

    increase wait for measuring test to get more consistent spacing

--- measure_test.py
+++ measure_test.py
@@ -12,7 +12,7 @@ try:
         loco.tightLeft()
         time.sleep(0.01)
         loco.stop()
-        time.sleep(0.02)
+        time.sleep(0.2)
         dist = sense.measure()
         print(dist)
         result +=","
