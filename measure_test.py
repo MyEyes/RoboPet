e8884c3694a0f4abd63f5ce6e2d293930a74fff6
Nils Ole Timm <nils.ole.timm@googlemail.com>
2021-05-29 16:46:09 +0200

    Outputting csv in measurement test

--- measure_test.py
+++ measure_test.py
@@ -5,10 +5,17 @@ from sensor import Sensor
 
 loco = Locomotion()
 sense = Sensor(config.SENSOR1_TRIGGER, config.SENSOR1_ECHO, config.SENSOR1_MAXDIST)
+result = ""
 
-while True:
-    loco.tightLeft()
-    time.sleep(0.01)
-    loco.stop()
-    time.sleep(0.02)
-    print(sense.measure())
+try:
+    while True:
+        loco.tightLeft()
+        time.sleep(0.01)
+        loco.stop()
+        time.sleep(0.02)
+        dist = sense.measure()
+        print(dist)
+        result +=","
+        result += str(dist)
+except:
+    print(result)
