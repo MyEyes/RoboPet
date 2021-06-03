2e15e3ec2cb5609cc4e29970cf62fa5709d5c0f1
Nils Ole Timm <nils.ole.timm@googlemail.com>
2021-06-03 17:46:07 +0200

    first auto robot test

--- robot.py
+++ robot.py
@@ -1,8 +1,25 @@
 from locomotion import Locomotion
+from sensor import Sensor
+import config
+import time
 
 def robot_run():
     locomotion = Locomotion()
-
+    sensor = Sensor(config.SENSOR1_TRIGGER, config.SENSOR1_ECHO, config.SENSOR1_MAXDIST)
+    while True:
+        dist = sensor.measure()
+        while dist>0.1:
+            locomotion.forward(0.7)
+            dist = sensor.measure()
+        locomotion.stop()
+        maxDist = dist
+        while dist < 1:
+            locomotion.tightRight()
+            time.sleep(0.1)
+            locomotion.stop()
+            dist = sensor.measure()
+            if dist>maxDist:
+                maxDist = dist
 
 if __file__ == "main":
     robot_run()
