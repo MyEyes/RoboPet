4f0fbb65d181fcfdfc42ae3370962595e3e31f79
Nils Ole Timm <nils.ole.timm@googlemail.com>
2021-06-04 00:10:42 +0200

    Try at automation with more sensors

--- robot.py
+++ robot.py
@@ -5,23 +5,41 @@ import time
 
 MIN_DIST = 0.2
 
+def minDist(senseL, senseC, senseR):
+    distL = senseL.measure()
+    distR = senseR.measure()
+    distC = senseC.measure()
+    if distL and distL<distR and distL<distC:
+        return distL
+    if distR and distR<distL and distR<distC:
+        return distR
+    if distC:
+        return distC
+    return None
+
 def robot_run():
     locomotion = Locomotion()
     sensor = Sensor(config.SENSOR1_TRIGGER, config.SENSOR1_ECHO, config.SENSOR1_MAXDIST)
+    senseL = Sensor(config.SENSOR_L_TRIGGER, config.SENSOR_L_ECHO, config.SENSOR_L_MAXDIST)
+    senseR = Sensor(config.SENSOR_R_TRIGGER, config.SENSOR_R_ECHO, config.SENSOR_R_MAXDIST)
     while True:
-        dist = sensor.measure()
+        dist = minDist(senseL, sensor, senseR)
         while dist and dist > MIN_DIST:
             locomotion.forward(0.7)
-            dist = sensor.measure()
+            dist = minDist(senseL, sensor, senseR)
         locomotion.stop()
-        maxDist = dist
+
         while dist and dist < 1:
-            locomotion.tightRight(0.7)
+
+            distL = senseL.measure()
+            distR = senseR.measure()
+            if(distL<distR):
+                locomotion.tightRight(0.7)
+            else:
+                locomotion.tightLeft(0.7)
             time.sleep(0.1)
             locomotion.stop()
             dist = sensor.measure()
-            if dist and dist>maxDist:
-                maxDist = dist
 
 if __name__ == "__main__":
     robot_run()
