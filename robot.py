fe6d097336a94e4d897fc70ae2fe881c36c034e2
Nils Ole Timm <nils.ole.timm@googlemail.com>
2021-06-03 17:53:11 +0200

    adjusting parameters

--- robot.py
+++ robot.py
@@ -3,18 +3,20 @@ from sensor import Sensor
 import config
 import time
 
+MIN_DIST = 0.2
+
 def robot_run():
     locomotion = Locomotion()
     sensor = Sensor(config.SENSOR1_TRIGGER, config.SENSOR1_ECHO, config.SENSOR1_MAXDIST)
     while True:
         dist = sensor.measure()
-        while dist and dist>0.1:
+        while dist and dist > MIN_DIST:
             locomotion.forward(0.7)
             dist = sensor.measure()
         locomotion.stop()
         maxDist = dist
         while dist and dist < 1:
-            locomotion.tightRight()
+            locomotion.tightRight(0.7)
             time.sleep(0.1)
             locomotion.stop()
             dist = sensor.measure()
